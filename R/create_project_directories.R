23cecdd3c059683af438b732483355b4fe920104
Joe Powers <joepowers16@gmail.com>
2019-04-17 23:02:58 -0700

    adds project_name to create_project_directories()

--- R/create_project_directories.R
+++ R/create_project_directories.R
@@ -1,15 +1,24 @@
 #' Creates the directories for a typical EDA project
 #'
 #' Creates the directories for a typical EDA project
+#' @param project_name a string representing the name of the Rproject
 #' @keywords organization workflow eda
 #' @export
 #' @examples
 #' create_project_directories()
 
-create_project_directories <- function(){
-  eda_template <- c("data", "data/raw", "munge", "analysis", "R")
+create_project_directories <- function(project_name){
+  project_name_data <- paste0("data/", project_name, "_cloud_data")
+
+  # build directories
+  eda_template <- c(
+      "data", project_name_data, paste(project_name_data, "raw", sep = "/"),
+      "munge", "analysis", "R"
+  )
+
   sapply(eda_template, dir.create)
-  file.create("file_paths.R") # File to save file paths
+
+  file.create("file_paths.R") # File in which to save file paths
 }
 
 
