655965cfe342fe7f4b9b8c3c32eab9e83941c9b0
Joe Powers <joepowers16@gmail.com>
2019-04-23 15:43:38 -0700

    adds temp_foo()

---
+++ R/temp_foo.R
@@ -0,0 +1,15 @@
+#' Say what this function does in one line
+#'
+#' Say what this function does in greater detail if that helps.
+#' @param data is the data
+#' @param x is a variable representing something
+#' @keywords organization efficiency
+#' @export
+#' @examples
+#' temp_foo(mtcars, x = mpg)
+
+temp_foo <- function(data, x){
+  x <- enquo(x)
+  data %>% pull(!!x) %>% mean()
+}
+
