16ad3142f42de68124515775df91f764bdbc03e7
Joe Powers <joepowers16@gmail.com>
2019-04-29 13:49:41 -0700

    updates file_paths.R to use fs::path()

--- file_paths.R
+++ file_paths.R
@@ -1,8 +1,9 @@
+library(fs)
 
 dir_raw <- here::here("breadcrumbs_cloud", "raw")
 dir_data <- here::here("breadcrumbs_cloud", "data")
 
-file_ds_surveys <- paste(dir_data, "ds_surveys.rds", sep = "/")
-file_ds_grades <- paste(dir_data, "ds_grades.rds", sep = "/")
-file_ds_state_tests <- paste(dir_data, "ds_state_tests.rds", sep = "/")
-file_ds_demographics <- paste(dir_data, "ds_demographics.rds", sep = "/")
+file_ds_surveys <- path(dir_data, "ds_surveys.rds")
+file_ds_grades <- path(dir_data, "ds_grades.rds")
+file_ds_state_tests <- path(dir_data, "ds_state_tests.rds")
+file_ds_demographics <- path(dir_data, "ds_demographics.rds")
