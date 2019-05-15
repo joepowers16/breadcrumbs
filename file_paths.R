4ccc8eaf954c95d7f26f13d31a52e094d54af818
Joe Powers <joepowers16@gmail.com>
2019-05-15 11:57:35 -0700

    deletes file_paths.R

--- file_paths.R
+++
@@ -1,9 +0,0 @@
-library(fs)
-
-dir_raw <- here::here("breadcrumbs_cloud", "raw")
-dir_data <- here::here("breadcrumbs_cloud", "data")
-
-file_ds_surveys <- path(dir_data, "ds_surveys.rds")
-file_ds_grades <- path(dir_data, "ds_grades.rds")
-file_ds_state_tests <- path(dir_data, "ds_state_tests.rds")
-file_ds_demographics <- path(dir_data, "ds_demographics.rds")
