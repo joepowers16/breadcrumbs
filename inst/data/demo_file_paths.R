5c95a2c5c2003e0f2f3c6c0497e6045b969c53cb
Joe Powers <joepowers16@gmail.com>
2019-06-21 11:07:13 -0700

    moves templates to correct directories

---
+++ inst/data/demo_file_paths.R
@@ -0,0 +1,40 @@
+#' Overview:
+#' This file contains a list of relative file paths that can be called from any script
+#' within the project directory.
+# library(fs)
+# library(readr)
+#
+# dir_raw <- here::here("breadcrumbs_cloud", "raw")
+# dir_data <- here::here("breadcrumbs_cloud", "data")
+#
+# file_ds_surveys <- path(dir_data, "ds_surveys.rds")
+# file_ds_grades <- path(dir_data, "ds_grades.rds")
+# file_ds_state_tests <- path(dir_data, "ds_state_tests.rds")
+# file_ds_demographics <- path(dir_data, "ds_demographics.rds")
+#
+# setwd("/Users/joepowers/Documents/coding/breadcrumbs/breadcrumbs_cloud/raw/surveys/")
+# d <- read.csv("Qualtrics_2015.csv")
+#
+# d <- read_csv("/Users/joepowers/Documents/coding/breadcrumbs/breadcrumbs_cloud/raw/surveys/Qualtrics_2015.csv")
+#
+# d <- read_csv(here::here("breadcrumbs_cloud","raw","surveys","Qualtrics_2015.csv"))
+#
+# file_survey_2015 <- here::here("breadcrumbs_cloud","raw","surveys","Qualtrics_2015.csv")
+#
+# d <- read_csv(file_survey_2015)
+#
+# library(fs)
+#
+# dir_raw_surveys <- here::here("breadcrumbs_cloud", "raw", "surveys")
+#
+# file_raw_survey_2014 <- path(dir_raw_surveys, "2014_survey.csv")
+# file_raw_survey_2015 <- path(dir_raw_surveys, "Qualtrics_2015.csv")
+# file_raw_survey_2016 <- path(dir_raw_surveys, "2016_survey.csv")
+#
+# d <- read_csv("/Users/joepowers/Documents/coding/breadcrumbs/breadcrumbs_cloud/raw/surveys/Qualtrics_2015.csv")
+#
+# file_raw_survey_2015 <- "/Users/joepowers/Documents/coding/breadcrumbs/breadcrumbs_cloud/raw/surveys/Qualtrics_2015.csv"
+#
+# read_csv(file_raw_survey_2015)
+#
+# source("file_paths.R")
