5d1360e1d5adc894748fe3301739ef22a9bc4f1e
Joe Powers <joepowers16@gmail.com>
2019-08-17 14:46:25 -0700

    absorbs create_learning_directories() into create_project_directories()

--- R/create_project_directories.R
+++ R/create_project_directories.R
@@ -1,31 +1,56 @@
-#' Creates the directories for a typical EDA project
+#' Creates the standard set of subdirectories for a typical R project
 #'
-#' Creates the directories for a typical EDA project
-#' @param vignette a boolean variable indicating whether one folder should be named "vignette" rather than the default "analysis"
+#' Creates the standard set of subdirectories for a typical R project
+#' @param style string value indicating the style of subdirectories to create (c("default", "minimal"))
+#' @param vignette a boolean value indicating whether "analysis" directory should be named "vignette"
 #' @keywords organization workflow eda
 #' @export
 #' @examples
 #' create_project_directories()
 
-create_project_directories <- function(vignette = FALSE, ...){
+create_project_directories <- function(style = "default", vignette = FALSE, ...){
   # extract project name for labeling cloud directory
   project_name <- basename( rstudioapi::getActiveProject() )
 
   project_cloud_dir <- paste0("cloud_", project_name)
 
-  # build directories
-  eda_template <-
+  # define directories
+  default_template <-
     c(project_cloud_dir, "munge", "analysis", "R",
-      paste(project_cloud_dir, "reports", sep = "/"),
-      paste(project_cloud_dir, "data", sep = "/"),
-      paste(project_cloud_dir, "data", "raw", sep = "/")
+      fs::path(project_cloud_dir, "reports"),
+      fs::path(project_cloud_dir, "data"),
+      fs::path(project_cloud_dir, "data", "raw")
     )
+  learning_template <-
+    c(project_cloud_dir, "munge", "vignette", "R",
+      fs::path("vignette", "daily_log"),
+      fs::path("vignette", "exercises"),
+      fs::path("vignette", "summaries"),
+      fs::path(project_cloud_dir, "reports"),
+      fs::path(project_cloud_dir, "data"),
+      fs::path(project_cloud_dir, "data", "raw")
+    )
+  minimal_template <-
+    c("munge", "analysis", "data", fs::path("data", "raw"))
 
-  purrr::walk(eda_template, dir.create)
-
-  breadcrumbs::add_file_paths()
-  file.create("README.md")
-  breadcrumbs::add_gitignore()
+  # populate project directory with files and subdirectories
+  if (style == "default") {
+    purrr::walk(default_template, dir.create)
+    breadcrumbs::add_file_paths()
+    breadcrumbs::add_readme(style = style)
+    breadcrumbs::add_gitignore()
+    file.create("README.md")
+  }
+  if (style == "learning") {
+    purrr::walk(learning_template, dir.create)
+    breadcrumbs::add_file_paths()
+    breadcrumbs::add_readme(style = style)
+    breadcrumbs::add_gitignore()
+  }
+  if (style == "minimal") {
+    purrr::walk(minimal_template, dir.create)
+    breadcrumbs::add_readme(style = style)
+  }
 
   if (vignette == TRUE) {file.rename("analysis", "vignette")}
 }
