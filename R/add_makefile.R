9903bccb8c53ba64f54244e52986fd8806c22f0b
Joseph Powers <joe@Josephs-MBP.attlocal.net>
2019-12-22 14:36:15 -0800

    updates add_makefile() to include warning and named makefiles for subprojects.

--- R/add_makefile.R
+++ R/add_makefile.R
@@ -6,9 +6,36 @@
 #' @examples
 #' add_makefile()
 
-add_makefile <- function(path = here::here()){
-	file.copy(
-		from = system.file("resources/file_templates/Makefile", package = "breadcrumbs"),
-		to = path
-	)
+add_makefile <- function(makefile_type = "project", path = here::here()){
+		if(file.exists(fs::path(path, "Makefile"))) {
+			stop("A Makefile already exists in the directory where you are trying to add a Makefile. Please rename existing Makefile to avoid a conflict (e.g., 'old.makefile'). Then re-execute add_makefile()")
+		}
+
+		if(makefile_type == "project") {
+		file.copy(
+			from = system.file(
+				"resources/file_templates/Makefile",
+				package = "breadcrumbs"),
+			to = path
+		)
+	}
+
+		if(makefile_type == "master") {
+		file.copy(
+			from = system.file(
+				"resources/file_templates/master.makefile",
+				package = "breadcrumbs"),
+			to = fs::path(path, "Makefile")
+		)
+	}
+
+	# Rename subproject makefile template after moving
+	if(makefile_type == "subproject") {
+		file.copy(
+			from = system.file(
+				"resources/file_templates/subproject.makefile",
+				package = "breadcrumbs"),
+			to = fs::path(path, paste0(subproject, ".makefile"))
+		)
+	}
 }
